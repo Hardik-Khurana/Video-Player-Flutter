f8ae467213c56cbf93fd054700e864e266d42823
Hardik Khurana <66410876+Hardik-Khurana@users.noreply.github.com>
2020-10-18 11:36:27 +0530

    Video Player created in Flutter.

---
+++ lib/local/local.dart
@@ -0,0 +1,57 @@
+import 'package:Video_Player/local/chewie_prep/chewie_list_item.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:video_player/video_player.dart';
+
+class Local extends StatefulWidget {
+  @override
+  _LocalState createState() => _LocalState();
+}
+
+class _LocalState extends State<Local> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Video Player - Local',
+          style: GoogleFonts.raleway(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.italic),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'assets/video/Guru.mp4',
+            ),
+            looping: false,
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'assets/video/kurta.mp4',
+            ),
+            looping: false,
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'assets/video/Haaye.mp4',
+            ),
+            looping: false,
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'assets/video/Naina.mp4',
+            ),
+            looping: false,
+          ),
+        ],
+      ),
+    );
+  }
+}
